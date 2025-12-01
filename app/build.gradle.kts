d642a761e75a39b1c0d57fe8c0fd5fb54a78e8b4
Alessandro Ferrara <ferrara.alessandro@proton.me>
2025-12-01 16:32:53 +0100

    Aggiunta libreria Firebase per gestione autenticazione

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     implementation(libs.firebase.messaging)
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
 }
